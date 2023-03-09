ac7d942a93678f0bc9727bcfcf2266716faef939
CWCTBOY <qkrdlswo98@gmail.com>
2023-03-09 18:19:05 +0900

    feat: first commit☘️

---
+++ src/main/java/snust/sbsp/SbspApplication.java
@@ -0,0 +1,13 @@
+package snust.sbsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/snust/sbsp/SbspApplicationTests.java
@@ -0,0 +1,13 @@
+package snust.sbsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
