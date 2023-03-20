98b7cee0e275ca6dd139bff3ca84dbf71b342291
nathn00 <qkrdlswo98@gmail.com>
2023-03-21 06:15:17 +0900

    FIX - replace annotation and create constructor that receive Crew domain

--- src/main/java/snust/sbsp/company/dto/res/CompanyRes.java
+++ src/main/java/snust/sbsp/company/dto/res/CompanyRes.java
@@ -3,12 +3,22 @@ package snust.sbsp.company.dto.res;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import snust.sbsp.company.domain.Company;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class CompanyRes {
+
   private Long id;
+
   private String name;
+
   private String address;
+
+  public CompanyRes(Company company) {
+    this.id = company.getId();
+    this.name = company.getName();
+    this.address = company.getAddress();
+  }
 }
