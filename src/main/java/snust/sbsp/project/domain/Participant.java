6d911697bfb31d959e7bcc5f96ab44bcab2621ed
nathn00 <qkrdlswo98@gmail.com>
2023-03-21 06:13:50 +0900

    FIX - replace annotations [Builder, AllArgsConstructor]

--- src/main/java/snust/sbsp/project/domain/Participant.java
+++ src/main/java/snust/sbsp/project/domain/Participant.java
@@ -12,15 +12,19 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity(name = "participant")
 public class Participant {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   @Column(name = "role")
   @Enumerated(value = EnumType.STRING)
   private Role role;
+
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "crew_id")
   private Crew crew;
+
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "project_id")
   private Project project;
