93225ae1c450e2ad3df65f8d2903eaa4e6ecc079
nathn00 <qkrdlswo98@gmail.com>
2023-03-28 18:52:10 +0900

    REFACTOR - refactor company, auth services to sync with changed DTOs

---
+++ src/main/java/snust/sbsp/project/service/ProjectService.java
@@ -0,0 +1,20 @@
+package snust.sbsp.project.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import snust.sbsp.crew.domain.Crew;
+import snust.sbsp.project.dto.res.base.ProjectDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProjectService {
+  @Transactional(readOnly = true)
+  public List<ProjectDto> readProjectList(Crew crew) {
+    return crew.getParticipantList()
+      .stream()
+      .map(participant -> new ProjectDto(participant.getProject()))
+      .collect(Collectors.toList());
+  }
+}
