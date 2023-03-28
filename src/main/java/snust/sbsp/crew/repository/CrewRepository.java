3d01bdae1bdd205f458338f08807eb5bbb644253
nathn00 <qkrdlswo98@gmail.com>
2023-03-28 18:48:26 +0900

    FEAT - crew repo extends Specification interface to easily filter

--- src/main/java/snust/sbsp/crew/repository/CrewRepository.java
+++ src/main/java/snust/sbsp/crew/repository/CrewRepository.java
@@ -1,10 +1,11 @@
 package snust.sbsp.crew.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import snust.sbsp.crew.domain.Crew;
 
 import java.util.Optional;
 
-public interface CrewRepository extends JpaRepository<Crew, Long> {
+public interface CrewRepository extends JpaRepository<Crew, Long>, JpaSpecificationExecutor<Crew> {
   Optional<Crew> findByEmail(String email);
 }
