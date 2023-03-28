9a3b90b995a87538e8d829529e2689d7feea900c
nathn00 <qkrdlswo98@gmail.com>
2023-03-28 18:41:05 +0900

    FEAT - make enum types to get string value

--- src/main/java/snust/sbsp/crew/domain/type/Role.java
+++ src/main/java/snust/sbsp/crew/domain/type/Role.java
@@ -1,5 +1,17 @@
 package snust.sbsp.crew.domain.type;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Role {
-  SERVICE_ADMIN, COMPANY_ADMIN, USER
+  SERVICE_ADMIN("서비스 관리자"),
+  COMPANY_ADMIN("관리자"),
+  ORDER("발주처"),
+  SUPERVISOR("감리사"),
+  CONSTRUCTION("건설사"),
+  DESIGN("설계사");
+
+  private final String value;
 }
--- src/main/java/snust/sbsp/project/domain/type/DetailCtrType.java
+++ src/main/java/snust/sbsp/project/domain/type/DetailCtrType.java
@@ -1,5 +1,17 @@
 package snust.sbsp.project.domain.type;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum DetailCtrType {
-    LIV, OFC, CMR, IND, HSP, SCH, ETC
+  LIV("주거용 건축물"),
+  OFC("사무실용 건축물"),
+  CMR("상업용 건축물"),
+  IND("공업용 건축물"),
+  HSP("병원"),
+  SCH("학교"),
+  ETC("기타");
+  private final String value;
 }
