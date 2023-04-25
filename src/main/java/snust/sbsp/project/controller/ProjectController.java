83be7c1cc5b736f568a915fed682dbc28bc76d5f
nathn00 <qkrdlswo98@gmail.com>
2023-04-25 17:48:26 +0900

    FEAT - create project Controller and Service

--- src/main/java/snust/sbsp/project/controller/ProjectController.java
+++ src/main/java/snust/sbsp/project/controller/ProjectController.java
@@ -1,11 +1,55 @@
 package snust.sbsp.project.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import snust.sbsp.common.res.Response;
+import snust.sbsp.common.util.SessionUtil;
+import snust.sbsp.project.dto.req.ProjectReq;
+import snust.sbsp.project.dto.res.ProjectRes;
+import snust.sbsp.project.service.ProjectService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/project")
 public class ProjectController {
+
+  private final ProjectService projectService;
+
+  private final SessionUtil sessionUtil;
+
+  @PostMapping
+  public ResponseEntity<?> newProject(
+    @CookieValue(
+      value = "JSESSIONID"
+    ) String jSessionId,
+    HttpServletRequest request,
+    @RequestBody ProjectReq projectReq
+  ) {
+    Long crewId = sessionUtil.getInfo(jSessionId, request);
+    projectService.createProject(projectReq, crewId);
+
+    return Response.ok(HttpStatus.OK);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<ProjectRes>> getProjectList(
+    @CookieValue(
+      value = "JSESSIONID"
+    ) String jSessionId,
+    HttpServletRequest request,
+    @PathParam("companyId") Long companyId,
+    @PathParam("name") String name,
+    @PathParam("ctrClass") String ctrClass,
+    @PathParam("detailCtrClass") String detailCtrClass,
+    @PathParam("attendance") Boolean attendance
+  ) {
+
+    return Response.ok(HttpStatus.OK);
+  }
 }
--- src/main/java/snust/sbsp/project/service/ProjectService.java
+++ src/main/java/snust/sbsp/project/service/ProjectService.java
@@ -1,15 +1,65 @@
 package snust.sbsp.project.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import snust.sbsp.common.exception.CustomCommonException;
+import snust.sbsp.common.exception.ErrorCode;
+import snust.sbsp.company.domain.Company;
+import snust.sbsp.company.repository.CompanyRepository;
 import snust.sbsp.crew.domain.Crew;
+import snust.sbsp.crew.domain.type.Role;
+import snust.sbsp.crew.repository.CrewRepository;
+import snust.sbsp.project.domain.Project;
+import snust.sbsp.project.domain.type.CtrType;
+import snust.sbsp.project.domain.type.DetailCtrType;
+import snust.sbsp.project.dto.req.ProjectReq;
 import snust.sbsp.project.dto.res.base.ProjectDto;
+import snust.sbsp.project.repository.ProjectRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectService {
+
+  private final CompanyRepository companyRepository;
+
+  private final ProjectRepository projectRepository;
+
+  private final CrewRepository crewRepository;
+
+  @Transactional
+  public void createProject(
+    ProjectReq projectReq,
+    Long crewId
+  ) {
+    Crew crew = crewRepository.findById(crewId)
+      .orElseThrow(() -> new CustomCommonException(ErrorCode.CREW_NOT_FOUND));
+
+    Role role = crew.getRole();
+    if (!role.equals(Role.COMPANY_ADMIN) && !role.equals(Role.SERVICE_ADMIN))
+      throw new CustomCommonException(ErrorCode.FORBIDDEN);
+
+    Company company = role.equals(Role.COMPANY_ADMIN)
+      ? crew.getCompany()
+      : companyRepository.findById(projectReq.getCompanyId())
+      .orElseThrow(() -> new CustomCommonException(ErrorCode.COMPANY_NOT_FOUND));
+
+    Project project = Project.builder()
+      .name(projectReq.getName())
+      .startDate(projectReq.getStartDate())
+      .endDate(projectReq.getEndDate())
+      .ctrType(CtrType.from(projectReq.getCtrType()))
+      .detailCtrType(DetailCtrType.from(projectReq.getDetailCtrType()))
+      .thumbnailUrl(projectReq.getThumbnailUrl())
+      .floorUrl(projectReq.getFloorPlanUrl())
+      .company(company)
+      .build();
+
+    projectRepository.save(project);
+  }
 
   @Transactional(readOnly = true)
   public List<ProjectDto> readProjectList(Crew crew) {
