032ce8f50a63a3f168b471bd2a522131d4dd4280
nathn00 <qkrdlswo98@gmail.com>
2023-04-25 17:49:00 +0900

    FEAT - create base of specification for project

--- src/main/java/snust/sbsp/project/specification/ProjectSpecification.java
+++ src/main/java/snust/sbsp/project/specification/ProjectSpecification.java
@@ -1,2 +1,29 @@
-package snust.sbsp.project.specification;public class ProjectSpecification {
+package snust.sbsp.project.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+import snust.sbsp.company.domain.Company;
+import snust.sbsp.project.domain.Project;
+import snust.sbsp.project.domain.type.CtrType;
+import snust.sbsp.project.domain.type.DetailCtrType;
+
+public class ProjectSpecification {
+  public static Specification<Project> equalCompany(Company company) {
+    return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("company"), company));
+  }
+
+  public static Specification<Project> equalName(String name) {
+    return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("name"), name));
+  }
+
+  public static Specification<Project> equalCtrClass(CtrType ctrClass) {
+    return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("ctrClass"), ctrClass));
+  }
+
+  public static Specification<Project> equalDetailCtrClass(DetailCtrType detailCtrClass) {
+    return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("detailCtrClass"), detailCtrClass));
+  }
+
+//  public static Specification<Project> isAttendance(Boolean attendance) {
+//    return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("role"), role));
+//  }
 }
