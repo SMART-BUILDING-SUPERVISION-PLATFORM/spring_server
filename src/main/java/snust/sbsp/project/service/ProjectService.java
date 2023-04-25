4bf9bff50874ac7d596e4daf3267ad71e74d16ba
nathn00 <qkrdlswo98@gmail.com>
2023-04-26 00:04:33 +0900

    FEAT - create filter project list methods

--- src/main/java/snust/sbsp/project/service/ProjectService.java
+++ src/main/java/snust/sbsp/project/service/ProjectService.java
@@ -1,6 +1,7 @@
 package snust.sbsp.project.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import snust.sbsp.common.exception.CustomCommonException;
@@ -15,7 +16,9 @@ import snust.sbsp.project.domain.type.CtrType;
 import snust.sbsp.project.domain.type.DetailCtrType;
 import snust.sbsp.project.dto.req.ProjectReq;
 import snust.sbsp.project.dto.res.base.ProjectDto;
+import snust.sbsp.project.repository.ParticipantRepository;
 import snust.sbsp.project.repository.ProjectRepository;
+import snust.sbsp.project.specification.ProjectSpecification;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,9 +27,11 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ProjectService {
 
-  private final CompanyRepository companyRepository;
-
   private final ProjectRepository projectRepository;
+
+  private final ParticipantRepository participantRepository;
+
+  private final CompanyRepository companyRepository;
 
   private final CrewRepository crewRepository;
 
@@ -62,11 +67,39 @@ public class ProjectService {
   }
 
   @Transactional(readOnly = true)
-  public List<ProjectDto> readProjectList(Crew crew) {
-    
-    return crew.getParticipantList()
+  public List<ProjectDto> readAllProjectList(
+    Long companyId,
+    String name,
+    String ctrClass,
+    String detailCtrClass
+  ) {
+    Specification<Project> specification = ((root, query, criteriaBuilder) -> null);
+    if (name != null)
+      specification = specification.and(ProjectSpecification.equalName(name));
+    if (ctrClass != null)
+      specification = specification.and(ProjectSpecification.equalCtrClass(CtrType.from(ctrClass)));
+    if (detailCtrClass != null)
+      specification = specification.and(ProjectSpecification.equalDetailCtrClass(DetailCtrType.from(detailCtrClass)));
+    if (companyId != null) {
+      Company company = companyRepository.findById(companyId)
+        .orElseThrow(() -> new CustomCommonException(ErrorCode.COMPANY_NOT_FOUND));
+      specification = specification.and(ProjectSpecification.equalCompany(company));
+    }
+
+    return projectRepository.findAll(specification)
       .stream()
-      .map(participant -> new ProjectDto(participant.getProject()))
+      .map((ProjectDto::new))
+      .collect(Collectors.toList());
+  }
+
+  @Transactional(readOnly = true)
+  public List<ProjectDto> readMyProjectList(Long crewId) {
+    Crew foundedCrew = crewRepository.findById(crewId)
+      .orElseThrow(() -> new CustomCommonException(ErrorCode.CREW_NOT_FOUND));
+
+    return foundedCrew.getParticipantList()
+      .stream()
+      .map((participant -> new ProjectDto(participant.getProject())))
       .collect(Collectors.toList());
   }
 }
