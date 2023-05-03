4d1be963a36f55aa93277525f4b1eae30a2beb63
nathn00 <qkrdlswo98@gmail.com>
2023-05-03 20:47:54 +0900

    FEAT - create Interceptor that the request is valid or not in session

---
+++ src/main/java/snust/sbsp/common/interceptor/Interceptor.java
@@ -0,0 +1,50 @@
+package snust.sbsp.common.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.HandlerInterceptor;
+import snust.sbsp.common.exception.CustomCommonException;
+import snust.sbsp.common.exception.ErrorCode;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+public class Interceptor implements HandlerInterceptor {
+
+  public static final String CURRENT_CREW_ID = "currentCrewId";
+
+  @Override
+  public boolean preHandle(
+    HttpServletRequest request,
+    HttpServletResponse response,
+    Object handler
+  ) throws Exception {
+    Cookie cookie;
+
+    try {
+      if (request.getCookies() == null) throw new CustomCommonException(ErrorCode.SESSION_NOT_FOUND);
+
+      cookie = Arrays.stream(request.getCookies())
+        .filter(c -> c.getName().equals("JSESSIONID"))
+        .findAny()
+        .orElseThrow(() -> new CustomCommonException(ErrorCode.SESSION_NOT_FOUND));
+    } catch (NullPointerException e) {
+      throw new CustomCommonException(ErrorCode.SESSION_NOT_FOUND);
+    }
+
+    String jSessionId = cookie.getValue();
+
+    HttpSession session = request.getSession();
+    Long currentCrewId = (Long) session.getAttribute(jSessionId);
+
+    if (currentCrewId == null)
+      throw new CustomCommonException(ErrorCode.SESSION_NOT_FOUND);
+
+    request.setAttribute(CURRENT_CREW_ID, currentCrewId);
+
+    return true;
+  }
+}
