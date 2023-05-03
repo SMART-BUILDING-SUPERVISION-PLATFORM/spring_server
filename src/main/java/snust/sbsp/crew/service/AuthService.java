4b8d765da273a486a5ba47f0786f03201fdc9616
nathn00 <qkrdlswo98@gmail.com>
2023-05-03 20:51:32 +0900

    IGNORE

--- src/main/java/snust/sbsp/crew/service/AuthService.java
+++ src/main/java/snust/sbsp/crew/service/AuthService.java
@@ -33,7 +33,7 @@ public class AuthService {
   public void signUp(SignUpReq signupReq) {
     Long companyId = signupReq.getCompanyId();
     Company company = companyService.findById(companyId);
-    String newCode = signupReq.getNewCode();
+    String newCode = signupReq.getValidationCode();
 
     if (!newCode.equals(redisUtil.getData(signupReq.getEmail())))
       throw new CustomCommonException(ErrorCode.FORBIDDEN);
