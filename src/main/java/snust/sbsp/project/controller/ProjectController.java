ce0189e7367ccb764b69052ee04a665f29ce8a05
nathn00 <qkrdlswo98@gmail.com>
2023-05-03 20:50:21 +0900

    FIX - remove cookie parameters into HttpAttribute following created Interceptor

--- src/main/java/snust/sbsp/project/controller/ProjectController.java
+++ src/main/java/snust/sbsp/project/controller/ProjectController.java
@@ -4,13 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import snust.sbsp.common.interceptor.Interceptor;
 import snust.sbsp.common.res.Response;
-import snust.sbsp.common.util.SessionUtil;
 import snust.sbsp.project.dto.req.ProjectReq;
 import snust.sbsp.project.dto.res.base.ProjectDto;
 import snust.sbsp.project.service.ProjectService;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -20,39 +19,29 @@ public class ProjectController {
 
   private final ProjectService projectService;
 
-  private final SessionUtil sessionUtil;
-
   @PostMapping
   public ResponseEntity<?> newProject(
-    @CookieValue(
-      value = "JSESSIONID"
-    ) String jSessionId,
-    HttpServletRequest request,
+    @RequestAttribute(Interceptor.CURRENT_CREW_ID) Long currentCrewId,
     @RequestBody ProjectReq projectReq
   ) {
-    Long crewId = sessionUtil.getInfo(jSessionId, request);
-    projectService.createProject(projectReq, crewId);
+    projectService.createProject(projectReq, currentCrewId);
 
     return Response.ok(HttpStatus.OK);
   }
 
   @GetMapping
   public ResponseEntity<List<ProjectDto>> getProjectList(
-    @CookieValue(
-      value = "JSESSIONID"
-    ) String jSessionId,
-    HttpServletRequest request,
+    @RequestAttribute(Interceptor.CURRENT_CREW_ID) Long currentCrewId,
     @RequestParam(required = false, value = "companyId") Long companyId,
     @RequestParam(required = false, value = "name") String name,
     @RequestParam(required = false, value = "ctrClass") String ctrClass,
     @RequestParam(required = false, value = "detailCtrClass") String detailCtrClass,
     @RequestParam(required = false, value = "onlyMine") Boolean onlyMine
   ) {
-    Long crewId = sessionUtil.getInfo(jSessionId, request);
     List<ProjectDto> projectList;
 
     if (onlyMine)
-      projectList = projectService.readMyProjectList(crewId);
+      projectList = projectService.readMyProjectList(currentCrewId);
     else
       projectList = projectService.readAllProjectList(companyId, name, ctrClass, detailCtrClass);
 
